feb239a7d63e0fa8abd9d79e4a17e8d738e268ef
Lei Chen <leichen@wustl.edu>
2020-02-04 13:06:59 -0600

    mask the cluster path

--- candidate_analysis_pipeline/8-calculate_flanking_snp_pval.sh
+++ candidate_analysis_pipeline/8-calculate_flanking_snp_pval.sh
@@ -5,8 +5,8 @@ echo $VCF $TRAIT
 
 VCF=${VCF_DIR}/snp*vcf.gz
 
-PHENO_FILE=/gscmnt/gc2802/halllab/lchen/finmetseq_final_20190604/2-Association_test/data/general/qt_finnseq_20190620.renormalized.wgs.5k_ids.power80.ped
-KIN_MAT=/gscmnt/gc2802/halllab/lganel/FinMetSeq5k/kinship.kinf
+#PHENO_FILE=
+#KIN_MAT=
 export LD_LIBRARY_PATH=/gscmnt/gc2719/halllab/src/gcc-4.9.2/lib64:$LD_LIBRARY_PATH
 EPACTS=/gscmnt/gc2719/halllab/users/lganel/src/EPACTS-3.2.9/bin/epacts 
 OUT_DIR=${VCF_DIR}/snp_assoc/${TRAIT}
